17717478193bd4f2b3afd0c4192ea758a74d8bcb
Alexander Potapenko <belyanskii@yandex-team.ru>
2020-06-23 15:41:06 +0300

    fix positioning test

--- src/vendor_upstream/dom/translateDOMPositionXY.js
+++ src/vendor_upstream/dom/translateDOMPositionXY.js
@@ -12,36 +12,16 @@
 
 'use strict';
 
-import BrowserSupportCore from 'BrowserSupportCore';
-
 import getVendorPrefixedName from 'getVendorPrefixedName';
 
 var TRANSFORM = getVendorPrefixedName('transform');
 var BACKFACE_VISIBILITY = getVendorPrefixedName('backfaceVisibility');
 
 var translateDOMPositionXY = (function() {
-  if (BrowserSupportCore.hasCSSTransforms()) {
-    var ua = global.window ? global.window.navigator.userAgent : 'UNKNOWN';
-    var isSafari = (/Safari\//).test(ua) && !(/Chrome\//).test(ua);
-    // It appears that Safari messes up the composition order
-    // of GPU-accelerated layers
-    // (see bug https://bugs.webkit.org/show_bug.cgi?id=61824).
-    // Use 2D translation instead.
-    if (!isSafari && BrowserSupportCore.hasCSS3DTransforms()) {
-      return function(/*object*/ style, /*number*/ x, /*number*/ y) {
-        style[TRANSFORM] = 'translate3d(' + x + 'px,' + y + 'px,0)';
-      };
-    } else {
-      return function(/*object*/ style, /*number*/ x, /*number*/ y) {
-        style[TRANSFORM] = 'translate(' + x + 'px,' + y + 'px)';
-      };
-    }
-  } else {
-    return function(/*object*/ style, /*number*/ x, /*number*/ y) {
-      style.left = x + 'px';
-      style.top = y + 'px';
-    };
-  }
+  return function(/*object*/ style, /*number*/ x, /*number*/ y) {
+    style.left = x + 'px';
+    style.top = y + 'px';
+  };
 })();
 
 export default translateDOMPositionXY;
